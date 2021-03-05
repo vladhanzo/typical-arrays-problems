18fafbe5b075561495bd053f8639090397bb2b2e
Владислав Капустин <70846638+vladhanzo@users.noreply.github.com>
2021-03-05 15:23:45 +0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length === 0) {
+        return 0;
+    }
+    return Math.min(...array);
+};
+exports.max = function max(array) {
+    if (array == undefined || array.length === 0) {
+        return 0;
+    }
+    return Math.max(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((a, b) => a + b, 0) / array.length;
+};
